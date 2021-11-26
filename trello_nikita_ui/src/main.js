07673eff2aadc63dc11adf754ef5fee52947784c
Nikita Tulupov <nekit1917@gmail.com>
2021-11-26 18:03:25 +0200

    rework develop branch2

--- trello_nikita_ui/src/main.js
+++ trello_nikita_ui/src/main.js
@@ -1,26 +1,22 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import { createApp } from 'vue'
-import App from './App.vue'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import PortalVue from 'portal-vue'
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import { createApp } from 'vue';
+import App from './App.vue';
+import Vue from 'vue';
+import './plugins/bootstrap-vue';
+import PortalVue from 'portal-vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(PortalVue)
-
-createApp(App).mount('#app')
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
+createApp(App).mount('#app')
 
 new Vue({
   render: h => h(App),
